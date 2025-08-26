2cb6d1db4d6bca6abba00be57f605337303d59bf
kisara71 <2517167786@qq.com>
2025-08-26 18:47:09 +0800

    refactor grpcx with etcd

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/prometheus/client_golang v1.23.0
 	github.com/redis/go-redis/v9 v9.12.1
+	go.etcd.io/etcd/client/v3 v3.6.4
 	go.uber.org/zap v1.27.0
 	google.golang.org/grpc v1.74.2
 )
@@ -18,6 +19,8 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/eapache/go-resiliency v1.7.0 // indirect
@@ -29,7 +32,10 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
@@ -54,12 +60,15 @@ require (
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.6.4 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.6.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250528174236-200df99c418a // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- grpcx/server.go
+++ grpcx/server.go
@@ -1,20 +1,101 @@
 package grpcx
 
 import (
+	"context"
+	"go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"google.golang.org/grpc"
 	"net"
+	"time"
 )
 
 type GrpcxServer struct {
 	*grpc.Server
-	Addr   string
+	Addr     string
+	Protocol string
+	Name     string
+	TTL      int64
+	key      string
+	client   *clientv3.Client
+	kaCancel func()
+	em       endpoints.Manager
+}
+
+type Config struct {
+	TTL      int64
+	Name     string
+	Addr     string
 	Protocol string
 }
 
+func NewGrpcxServer(client *clientv3.Client, server *grpc.Server, cfg Config) *GrpcxServer {
+	return &GrpcxServer{
+		Server:   server,
+		client:   client,
+		TTL:      cfg.TTL,
+		Name:     cfg.Name,
+		Protocol: cfg.Protocol,
+		Addr:     cfg.Addr,
+	}
+}
+
 func (g *GrpcxServer) Serve() error {
-	l, err := net.Listen(g.Addr, g.Protocol)
+	l, err := net.Listen(g.Protocol, g.Addr)
+	if err != nil {
+		return err
+	}
+	err = g.register()
 	if err != nil {
 		return err
 	}
 	return g.Server.Serve(l)
+}
+
+func (g *GrpcxServer) register() error {
+	em, err := endpoints.NewManager(g.client, "service/"+g.Name)
+	if err != nil {
+		return err
+	}
+	g.em = em
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	lease, err := g.client.Grant(ctx, g.TTL)
+	cancel()
+	if err != nil {
+		return err
+	}
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	g.key = "service/" + g.Name + "/" + g.Addr
+	err = em.AddEndpoint(ctx, g.key, endpoints.Endpoint{
+		Addr: g.Addr,
+	}, clientv3.WithLease(lease.ID))
+	cancel()
+	if err != nil {
+		return err
+	}
+	kaCtx, kaCancel := context.WithCancel(context.Background())
+	g.kaCancel = kaCancel
+	ch, err := g.client.KeepAlive(kaCtx, lease.ID)
+	if err != nil {
+		return err
+	}
+	go func() {
+		for _ = range ch {
+		}
+	}()
+	return nil
+}
+func (g *GrpcxServer) Close() error {
+	if g.kaCancel != nil {
+		g.kaCancel()
+	}
+	if g.em != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err := g.em.DeleteEndpoint(ctx, g.key)
+		if err != nil {
+			return err
+		}
+	}
+	g.Server.GracefulStop()
+	return nil
 }
